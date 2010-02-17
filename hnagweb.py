de9bbfe54513150bdb9094499d02a58c01b363ae
Hareesh Nagarajan <hareesh.nagarajan@gmail.com>
2010-02-16 17:48:44 -0800

    init

---
+++ hnagweb.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# Copyright 2010 Hareesh Nagarajan.
+
+__author__ = 'hareesh.nagarajan@gmail.com (Hareesh Nagarajan)'
+
+import os
+import wsgiref.handlers
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.api import memcache
+
+class Home(webapp.RequestHandler):
+  def get(self):
+    path = os.path.join(os.path.dirname(__file__), 'index.html')
+    self.response.out.write(template.render(path, {}))
+
+def main():
+  application = webapp.WSGIApplication([
+      ('/', Home),
+      ], debug=True)
+  wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == '__main__':
+  main()
