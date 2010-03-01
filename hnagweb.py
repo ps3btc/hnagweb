8af56de74126df4dc59f77b6308604960e4312d9
Hareesh Nagarajan <hareesh.nagarajan@gmail.com>
2010-03-01 11:10:16 -0800

    x

--- hnagweb.py
+++ hnagweb.py
@@ -5,25 +5,55 @@
 __author__ = 'hareesh.nagarajan@gmail.com (Hareesh Nagarajan)'
 
 import os
+import logging
 import wsgiref.handlers
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.api import memcache
 
+HOMEPAGE_KEY = "homepage"
+CREDITS_KEY = "credits"
+MEMCACHE_EXPIRE = 864000
+
+class FlushMemcache(webapp.RequestHandler):
+  def get(self):
+    if memcache.flush_all():
+      logging.info('memcache: flushed all')
+      self.redirect('/', permanent=False)
+
 class Home(webapp.RequestHandler):
   def get(self):
+    homepage = memcache.get(HOMEPAGE_KEY)
+    if homepage:
+      logging.info('found homepage in memcache')
+      self.response.out.write(homepage)
+      return
+    
     path = os.path.join(os.path.dirname(__file__), 'index.html')
+    template_values = {}
+    render_page = template.render(path, template_values)
+    memcache.add(HOMEPAGE_KEY, render_page, MEMCACHE_EXPIRE)
     self.response.out.write(template.render(path, {}))
-
+     
 class Credits(webapp.RequestHandler):
   def get(self):
+    page = memcache.get(CREDITS_KEY)
+    if page:
+      logging.info('found credits in memcache')
+      self.response.out.write(page)
+      return
+    
     path = os.path.join(os.path.dirname(__file__), 'credits.html')
+    template_values = {}
+    render_page = template.render(path, template_values)
+    memcache.add(CREDITS_KEY, render_page, MEMCACHE_EXPIRE)
     self.response.out.write(template.render(path, {}))
 
 def main():
   application = webapp.WSGIApplication([
       ('/', Home),
       ('/credits', Credits),
+      ('/flushmemcache', FlushMemcache),
       ], debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
